bddd154efff4089a5f87ce18187920389a0f0d93
Moriafly <moriafly@163.com>
2023-07-12 12:51:15 +0800

    Target SDK 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.moriafly.salt.app"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
