8a952375121d7417cfc767eb6f59c37461b0b470
Moriafly <moriafly@163.com>
2023-07-12 16:14:24 +0800

    Create RoundedColumn.kt

---
+++ ui/src/main/java/com/moriafly/salt/ui/RoundedColumn.kt
@@ -0,0 +1,48 @@
+@file:Suppress("UNUSED")
+
+/**
+ * SaltUI
+ * Copyright (C) 2023 Moriafly
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package com.moriafly.salt.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Column with rounded radius
+ */
+@Composable
+fun RoundedColumn(
+    color: Color = SaltTheme.colors.subBackground,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(Dimens.outerHorizontalPadding, Dimens.outerVerticalPadding)
+            .clip(RoundedCornerShape(Dimens.corner))
+            .background(color)
+    ) {
+        content()
+    }
+}
