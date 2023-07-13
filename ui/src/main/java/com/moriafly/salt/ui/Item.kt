46b6ec6d26f8550d0e4aa51506ab1917f01b6ff5
Moriafly <moriafly@163.com>
2023-07-13 20:34:56 +0800

    添加 Item 控件

--- ui/src/main/java/com/moriafly/salt/ui/Item.kt
+++ ui/src/main/java/com/moriafly/salt/ui/Item.kt
@@ -17,14 +17,27 @@
 
 package com.moriafly.salt.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 /**
  * Build content interface title text.
@@ -71,4 +84,68 @@ fun ItemSpacer() {
         modifier = Modifier
             .height(Dimens.innerVerticalPadding)
     )
+}
+
+/**
+ * Build item for the content interface.
+ *
+ * @param onClick will be called when user clicks on the element
+ * @param iconPainter icon
+ * @param iconColor color of [iconPainter], if this value is null, will use the paint original color
+ * @param text main text
+ * @param sub sub text
+ */
+@Composable
+fun Item(
+    onClick: () -> Unit,
+    iconPainter: Painter? = null,
+    iconColor: Color? = null,
+    text: String,
+    sub: String? = null
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            }
+            .padding(horizontal = Dimens.innerHorizontalPadding, vertical = Dimens.innerVerticalPadding),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        iconPainter?.let {
+            Image(
+                modifier = Modifier
+                    .size(24.dp),
+                painter = iconPainter,
+                contentDescription = null,
+                colorFilter = iconColor?.let { ColorFilter.tint(iconColor) }
+            )
+            Spacer(modifier = Modifier.width(Dimens.contentPadding))
+        }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = text,
+                style = SaltTheme.textStyles.main
+            )
+            sub?.let {
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = sub,
+                    style = SaltTheme.textStyles.sub
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(Dimens.contentPadding))
+        Icon(
+            modifier = Modifier
+                .size(20.dp),
+            painter = painterResource(id = R.drawable.ic_chevron_right),
+            contentDescription = null,
+            tint = SaltTheme.colors.subText
+        )
+    }
 }
