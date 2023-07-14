a78aca172c4410e9bd7711d4e81c47a1b46790a1
Moriafly <moriafly@163.com>
2023-07-14 09:34:06 +0800

    调整测试程序

--- app/src/main/java/com/moriafly/salt/app/MainActivity.kt
+++ app/src/main/java/com/moriafly/salt/app/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import com.moriafly.salt.ui.BottomBar
+import com.moriafly.salt.ui.BottomBarItem
 import com.moriafly.salt.ui.Item
 import com.moriafly.salt.ui.ItemContainer
 import com.moriafly.salt.ui.ItemSpacer
@@ -24,68 +30,158 @@ import com.moriafly.salt.ui.SaltTheme
 import com.moriafly.salt.ui.TextButton
 import com.moriafly.salt.ui.TitleBar
 import com.moriafly.salt.ui.UnstableSaltApi
+import com.moriafly.salt.ui.darkSaltColors
+import com.moriafly.salt.ui.lightSaltColors
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(UnstableSaltApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SaltTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = SaltTheme.colors.background)
-                ) {
-                    TitleBar(
-                        onBack = { /*TODO*/ },
-                        text = "SaltUI"
-                    )
-
-                    RoundedColumn {
-                        ItemTitle(text = "Hello, SaltUI")
-                        ItemText(
-                            text = "SaltUI（UI for Salt Player） 是提取自椒盐音乐的 UI 风格组件，用以快速生成椒盐音乐风格用户界面。本库将会广泛用以椒盐系列 App 开发，以达到快速开发目的"
-                        )
-                        ItemSpacer()
-                    }
-
-                    RoundedColumn {
-                        ItemTitle(text = "控件")
-                        Item(
-                            onClick = {
-
-                            },
-                            iconPainter = painterResource(id = R.drawable.ic_qr_code),
-                            iconColor = SaltTheme.colors.highlight,
-                            text = "标准 Item 控件，带图标（可选），副标题文本（可选）",
-                            sub = "Item 控件的副标题"
-                        )
-                        var switch by remember { mutableStateOf(false) }
-                        ItemSwitcher(
-                            state = switch,
-                            onChange = {
-                                switch = it
-                            },
-                            iconPainter = painterResource(id = R.drawable.ic_verified),
-                            iconColor = SaltTheme.colors.highlight,
-                            text = "标准开关控件，带图标（可选），副标题文本（可选）",
-                            sub = "开关控件的副标题"
-                        )
-                        ItemContainer {
-                            TextButton(
-                                onClick = {
-
-                                },
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                text = "默认按钮 TextButton"
-                            )
-                        }
-                    }
-                }
+            val colors = if (isSystemInDarkTheme()) {
+                darkSaltColors()
+            } else {
+                lightSaltColors()
+            }
+            SaltTheme(
+                colors = colors
+            ) {
+                MainUI()
             }
         }
     }
 
+}
+
+@OptIn(UnstableSaltApi::class)
+@Composable
+private fun MainUI() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        TitleBar(
+            onBack = {
+
+            },
+            text = "SaltUI"
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(color = SaltTheme.colors.background)
+                .verticalScroll(rememberScrollState())
+        ) {
+            RoundedColumn {
+                ItemTitle(text = "Hello, SaltUI")
+                ItemText(
+                    text = "SaltUI（UI for Salt Player） 是提取自椒盐音乐的 UI 风格组件，用以快速生成椒盐音乐风格用户界面。本库将会广泛用以椒盐系列 App 开发，以达到快速开发目的"
+                )
+                ItemSpacer()
+            }
+
+            RoundedColumn {
+                ItemTitle(text = "控件")
+                Item(
+                    onClick = {
+
+                    },
+                    iconPainter = painterResource(id = R.drawable.ic_qr_code),
+                    iconColor = SaltTheme.colors.highlight,
+                    text = "标准 Item 控件，带图标（可选），副标题文本（可选）",
+                    sub = "Item 控件的副标题"
+                )
+                var switch by remember { mutableStateOf(false) }
+                ItemSwitcher(
+                    state = switch,
+                    onChange = {
+                        switch = it
+                    },
+                    iconPainter = painterResource(id = R.drawable.ic_verified),
+                    iconColor = SaltTheme.colors.highlight,
+                    text = "标准开关控件，带图标（可选），副标题文本（可选）",
+                    sub = "开关控件的副标题"
+                )
+                ItemContainer {
+                    TextButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "默认按钮 TextButton"
+                    )
+                }
+            }
+
+            RoundedColumn {
+                ItemTitle(text = "其他样式测试")
+                Item(
+                    onClick = {
+
+                    },
+                    text = "标准 Item 控件",
+                    sub = "Item 控件的副标题"
+                )
+                Item(
+                    onClick = {
+
+                    },
+                    iconPainter = painterResource(id = R.drawable.ic_qr_code),
+                    iconColor = SaltTheme.colors.highlight,
+                    text = "标准 Item 控件"
+                )
+                Item(
+                    onClick = {
+
+                    },
+                    text = "标准 Item 控件"
+                )
+                ItemSwitcher(
+                    state = true,
+                    onChange = {
+
+                    },
+                    text = "标准开关控件",
+                    sub = "开关控件的副标题"
+                )
+                ItemSwitcher(
+                    state = true,
+                    onChange = {
+
+                    },
+                    iconPainter = painterResource(id = R.drawable.ic_verified),
+                    iconColor = SaltTheme.colors.highlight,
+                    text = "标准开关控件"
+                )
+                ItemSwitcher(
+                    state = true,
+                    onChange = {
+
+                    },
+                    text = "标准开关控件"
+                )
+            }
+        }
+        BottomBar {
+            BottomBarItem(
+                state = true,
+                onClick = {
+
+                },
+                painter = painterResource(id = R.drawable.ic_qr_code),
+                text = "二维码"
+            )
+            BottomBarItem(
+                state = false,
+                onClick = {
+
+                },
+                painter = painterResource(id = R.drawable.ic_verified),
+                text = "认证"
+            )
+        }
+    }
 }
