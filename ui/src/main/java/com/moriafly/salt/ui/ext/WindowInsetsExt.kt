9f590c7413693097dec3c4b7ab3b1737265c35e4
Moriafly <moriafly@163.com>
2024-01-22 13:14:58 +0800

    新增 WindowInsets.safeMain

---
+++ ui/src/main/java/com/moriafly/salt/ui/ext/WindowInsetsExt.kt
@@ -0,0 +1,33 @@
+@file:Suppress("UNUSED")
+
+/**
+ * SaltUI
+ * Copyright (C) 2024 Moriafly
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package com.moriafly.salt.ui.ext
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.union
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+
+/**
+ * WindowInsets.safeDrawing, Exclude ime
+ */
+val safeMain: WindowInsets
+    @Composable
+    @NonRestartableComposable
+    get() = WindowInsets.systemBars.union(WindowInsets.displayCutout)
