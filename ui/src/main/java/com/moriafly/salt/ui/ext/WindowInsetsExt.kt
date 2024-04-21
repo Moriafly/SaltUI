223e09bb641fff47227a57c3a968362cda5cfa02
Moriafly <moriafly@163.com>
2024-04-21 21:00:45 +0800

    新增 Modifier.safeMainPadding()

--- ui/src/main/java/com/moriafly/salt/ui/ext/WindowInsetsExt.kt
+++ ui/src/main/java/com/moriafly/salt/ui/ext/WindowInsetsExt.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.ui.Modifier
 import com.moriafly.salt.ui.UnstableSaltApi
 
 /**
@@ -33,3 +35,7 @@ val WindowInsets.Companion.safeMain: WindowInsets
     @Composable
     @NonRestartableComposable
     get() = WindowInsets.systemBars.union(WindowInsets.displayCutout)
+
+@OptIn(UnstableSaltApi::class)
+@Composable
+fun Modifier.safeMainPadding() = windowInsetsPadding(WindowInsets.safeMain)
