065065d6a297203af1a6f41c04b0890d2d288b55
Moriafly <moriafly@163.com>
2024-06-19 15:01:28 +0800

    2.0.0-dev04

--- ui2/build.gradle.kts
+++ ui2/build.gradle.kts
@@ -1,17 +1,69 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
     `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.28.0"
+}
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.moriafly",
+        artifactId = "salt-ui",
+        version = "2.0.0-dev04"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("Salt UI")
+        description.set("Moriafly Salt UI")
+        inceptionYear.set("2024")
+        url.set("https://github.com/Moriafly/SaltUI")
+
+        licenses {
+            license {
+                name.set("Apache-2.0 license")
+                url.set("https://github.com/Moriafly/SaltUI/blob/main/LICENSE")
+            }
+        }
+
+        // Specify developer information
+        developers {
+            developer {
+                id.set("Moriafly")
+                name.set("Moriafly")
+                email.set("moriafly@163.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/Moriafly/SaltUI")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
 
 kotlin {
+    withSourcesJar(publish = false)
+
     androidTarget {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "17"
             }
         }
+
+        publishLibraryVariants("release")
+        // publishLibraryVariantsGroupedByFlavor = true
     }
     
     jvm("desktop")
