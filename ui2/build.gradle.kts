8721dd7233bc76560797d4e02cf4a0bca58f3e25
Moriafly <moriafly@163.com>
2024-07-31 20:56:10 +0800

    2.0.1 Release

--- ui2/build.gradle.kts
+++ ui2/build.gradle.kts
@@ -13,7 +13,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.moriafly",
         artifactId = "salt-ui",
-        version = "2.0.0"
+        version = "2.0.1"
     )
 
     // Configure POM metadata for the published artifact
