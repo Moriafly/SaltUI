31cefc51e1b52a8ed4bb910e937c7bdab14ea856
Moriafly <moriafly@163.com>
2024-08-09 17:09:45 +0800

    Add Salt UI Surface component

---
+++ ui2/src/commonMain/kotlin/com/moriafly/salt/ui/Surface.kt
@@ -0,0 +1,49 @@
+/*
+ * SaltUI
+ * Copyright (C) 2024 Moriafly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UNUSED")
+
+package com.moriafly.salt.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.semantics.isContainer
+import androidx.compose.ui.semantics.semantics
+
+/**
+ * The Salt UI Surface is a component marked as a Container that prevents touch events from being passed down
+ */
+@UnstableSaltApi
+@Composable
+fun Surface(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .semantics(mergeDescendants = false) {
+                @Suppress("DEPRECATION")
+                isContainer = true
+            }
+            .pointerInput(Unit) {},
+        propagateMinConstraints = true
+    ) {
+        content()
+    }
+}
