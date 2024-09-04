3aabc6f686f5a09996c269d9428b64574aae599e
Moriafly <moriafly@163.com>
2024-09-04 15:54:30 +0800

    Improve BasicButton style

--- ui2/src/commonMain/kotlin/com/moriafly/salt/ui/Button.kt
+++ ui2/src/commonMain/kotlin/com/moriafly/salt/ui/Button.kt
@@ -22,8 +22,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,7 +33,6 @@ import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import kotlin.math.min
 
 /**
  * Default text content button
@@ -69,6 +66,8 @@ fun TextButton(
 
 /**
  * Basic button
+ *
+ * No min [SaltDimens.item] height limit
  */
 @Composable
 fun BasicButton(
@@ -80,7 +79,6 @@ fun BasicButton(
 ) {
     Box(
         modifier = modifier
-            .heightIn(min = SaltTheme.dimens.item)
             .semantics {
                 role = Role.Button
             }
