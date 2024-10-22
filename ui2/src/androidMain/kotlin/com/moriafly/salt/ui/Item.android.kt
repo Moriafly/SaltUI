8bc82688a01f288ae78905af8cd9c8bee0072e23
Moriafly <moriafly@163.com>
2024-10-22 10:43:12 +0800

    Format Item.android.kt code

--- ui2/src/androidMain/kotlin/com/moriafly/salt/ui/Item.android.kt
+++ ui2/src/androidMain/kotlin/com/moriafly/salt/ui/Item.android.kt
@@ -17,15 +17,10 @@
 
 package com.moriafly.salt.ui
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
