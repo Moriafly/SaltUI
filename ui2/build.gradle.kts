6ba689b5da9b8f4c6be0a5c8337c4956cac12b6d
Moriafly <moriafly@163.com>
2024-11-12 10:12:19 +0800

    Remove implementation(compose.material)

--- ui2/build.gradle.kts
+++ ui2/build.gradle.kts
@@ -94,7 +94,6 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
