0031eafb89f3a52b91f5c130b0dfe8870bed2094
Moriafly <moriafly@163.com>
2024-11-15 10:54:14 +0800

    Format ScreenUtil.kt

--- ui2/src/androidMain/kotlin/com/moriafly/salt/ui/util/ScreenUtil.kt
+++ ui2/src/androidMain/kotlin/com/moriafly/salt/ui/util/ScreenUtil.kt
@@ -28,26 +28,30 @@ import com.moriafly.salt.ui.UnstableSaltApi
 object ScreenUtil {
 
     /**
-     * Get the rounded corner radius for the top corners of the screen.
+     * Get the rounded corner radius for the top corners of the screen
      *
-     * The curvature of the rounded corners varies among various devices, and this radius attribute is also an approximate value.
-     * In actual implementation, further processing should be considered after obtaining this attribute,
-     * such as defaulting to smaller values to cover different curvatures.
+     * The curvature of the rounded corners varies among various devices, and this radius attribute
+     * is also an approximate value
      *
-     * @param context The context.
+     * In actual implementation, further processing should be considered after obtaining this
+     * attribute, such as defaulting to smaller values to cover different curvatures
      *
-     * @return The pixel value of the rounded corner radius for the top corners of the screen.
+     * @param context The context
      *
-     * @throws Resources.NotFoundException This attribute needs to be defined by the OEM manufacturer,
-     *      and there may be situations where it cannot be obtained.
+     * @return The pixel value of the rounded corner radius for the top corners of the screen
+     *
+     * @throws Resources.NotFoundException This attribute needs to be defined by the OEM
+     *   manufacturer, and there may be situations where it cannot be obtained
      */
     @SuppressLint("DiscouragedApi")
     fun getRoundedCornerRadiusTop(context: Context): Int {
-        val resourceId = context.resources.getIdentifier("rounded_corner_radius_top", "dimen", "android")
+        val resourceId = context.resources.getIdentifier(
+            "rounded_corner_radius_top", "dimen", "android"
+        )
         if (resourceId != 0) {
             return context.resources.getDimensionPixelSize(resourceId)
         } else {
-            throw Resources.NotFoundException("Resource id rounded_corner_radius_top not found.")
+            throw Resources.NotFoundException("Resource id rounded_corner_radius_top not found")
         }
     }
 
