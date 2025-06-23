a0646dc762908043208fbbc226494c3ea3747137
不要糖醋放椒盐 <62319450+Moriafly@users.noreply.github.com>
2025-06-23 10:22:13 +0800

    Update SaltWindowTest.kt

--- composeApp/src/desktopMain/kotlin/SaltWindowTest.kt
+++ composeApp/src/desktopMain/kotlin/SaltWindowTest.kt
@@ -7,15 +7,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowDecoration
 import androidx.compose.ui.window.application
 import com.moriafly.salt.ui.UnstableSaltUiApi
-import com.moriafly.salt.ui.window.SaltWindow
+import com.moriafly.salt.ui.window.SaltDialogWindow
 
 @OptIn(ExperimentalComposeUiApi::class, UnstableSaltUiApi::class)
 fun main() {
     application {
-        SaltWindow(
+        SaltDialogWindow(
             onCloseRequest = ::exitApplication,
+            decoration = WindowDecoration.Undecorated(),
             minSize = DpSize(200.dp, 200.dp)
         ) {
             Box(
