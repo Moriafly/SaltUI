78230e0d786baaff03fb58e1c3d55be705375599
不要糖醋放椒盐 <62319450+Moriafly@users.noreply.github.com>
2025-08-29 13:10:18 +0800

    Reformat KDoc parameter indentation in Text.kt
    Adjusted the indentation of KDoc @param descriptions for improved readability and consistency. No functional changes were made to the code.

--- ui2/src/commonMain/kotlin/com/moriafly/salt/ui/Text.kt
+++ ui2/src/commonMain/kotlin/com/moriafly/salt/ui/Text.kt
@@ -51,33 +51,33 @@ import androidx.compose.ui.unit.TextUnit
  * @param text the text to be displayed.
  * @param modifier [Modifier] to apply to this layout node.
  * @param color [Color] to apply to the text. If [Color.Unspecified], and [style] has no color set,
- *   this will be [SaltTheme] text color.
+ * this will be [SaltTheme] text color.
  * @param fontSize the size of glyphs to use when painting the text. See [TextStyle.fontSize]
  * @param fontStyle the typeface variant to use when drawing the letters (e.g., italic). See
- *   [TextStyle.fontStyle].
+ * [TextStyle.fontStyle].
  * @param fontWeight the typeface thickness to use when painting the text (e.g., [FontWeight.Bold]).
  * @param fontFamily the font family to be used when rendering the text. See [TextStyle.fontFamily].
  * @param letterSpacing the amount of space to add between each letter. See
- *   [TextStyle.letterSpacing].
+ * [TextStyle.letterSpacing].
  * @param textDecoration the decorations to paint on the text (e.g., an underline). See
- *   [TextStyle.textDecoration].
+ * [TextStyle.textDecoration].
  * @param textAlign the alignment of the text within the lines of the paragraph. See
- *   [TextStyle.textAlign].
+ * [TextStyle.textAlign].
  * @param lineHeight line height for the [Paragraph] in [TextUnit] unit, e.g. SP or EM. See
- *   [TextStyle.lineHeight].
+ * [TextStyle.lineHeight].
  * @param overflow how visual overflow should be handled.
  * @param softWrap whether the text should break at soft line breaks. If false, the glyphs in the
- *   text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
- *   [overflow] and TextAlign may have unexpected effects.
+ * text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
+ * [overflow] and TextAlign may have unexpected effects.
  * @param maxLines an optional maximum number of lines for the text to span, wrapping if
- *   necessary. If the text exceeds the given number of lines, it will be truncated according to
- *   [overflow] and [softWrap]. It is required that 1 <= [minLines] <= [maxLines].
+ * necessary. If the text exceeds the given number of lines, it will be truncated according to
+ * [overflow] and [softWrap]. It is required that 1 <= [minLines] <= [maxLines].
  * @param minLines the minimum height in terms of minimum number of visible lines. It is required
- *   that 1 <= [minLines] <= [maxLines].
+ * that 1 <= [minLines] <= [maxLines].
  * @param onTextLayout callback that is executed when a new text layout is calculated. A
- *   [TextLayoutResult] object that callback provides contains paragraph information, size of the
- *   text, baselines and other details. The callback can be used to add additional decoration or
- *   functionality to the text. For example, to draw selection around the text.
+ * [TextLayoutResult] object that callback provides contains paragraph information, size of the
+ * text, baselines and other details. The callback can be used to add additional decoration or
+ * functionality to the text. For example, to draw selection around the text.
  * @param style style configuration for the text such as color, font, line height etc.
  */
 @Composable
@@ -144,35 +144,35 @@ fun Text(
  * @param text the text to be displayed.
  * @param modifier [Modifier] to apply to this layout node.
  * @param color [Color] to apply to the text. If [Color.Unspecified], and [style] has no color set,
- *   this will be [SaltTheme] text color.
+ * this will be [SaltTheme] text color.
  * @param fontSize the size of glyphs to use when painting the text. See [TextStyle.fontSize].
  * @param fontStyle the typeface variant to use when drawing the letters (e.g., italic). See
- *   [TextStyle.fontStyle].
+ * [TextStyle.fontStyle].
  * @param fontWeight the typeface thickness to use when painting the text (e.g., [FontWeight.Bold]).
  * @param fontFamily the font family to be used when rendering the text. See [TextStyle.fontFamily].
  * @param letterSpacing the amount of space to add between each letter. See
- *   [TextStyle.letterSpacing].
+ * [TextStyle.letterSpacing].
  * @param textDecoration the decorations to paint on the text (e.g., an underline). See
- *   [TextStyle.textDecoration].
+ * [TextStyle.textDecoration].
  * @param textAlign the alignment of the text within the lines of the paragraph. See
- *   [TextStyle.textAlign].
+ * [TextStyle.textAlign].
  * @param lineHeight line height for the [Paragraph] in [TextUnit] unit, e.g. SP or EM. See
- *   [TextStyle.lineHeight].
+ * [TextStyle.lineHeight].
  * @param overflow how visual overflow should be handled.
  * @param softWrap whether the text should break at soft line breaks. If false, the glyphs in the
- *   text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
- *   [overflow] and TextAlign may have unexpected effects.
+ * text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
+ * [overflow] and TextAlign may have unexpected effects.
  * @param maxLines an optional maximum number of lines for the text to span, wrapping if
- *   necessary. If the text exceeds the given number of lines, it will be truncated according to
- *   [overflow] and [softWrap]. It is required that 1 <= [minLines] <= [maxLines].
+ * necessary. If the text exceeds the given number of lines, it will be truncated according to
+ * [overflow] and [softWrap]. It is required that 1 <= [minLines] <= [maxLines].
  * @param minLines the minimum height in terms of minimum number of visible lines. It is required
- *   that 1 <= [minLines] <= [maxLines].
+ * that 1 <= [minLines] <= [maxLines].
  * @param inlineContent a map store composables that replaces certain ranges of the text. It's
- *   used to insert composables into text layout. Check [InlineTextContent] for more information.
+ * used to insert composables into text layout. Check [InlineTextContent] for more information.
  * @param onTextLayout callback that is executed when a new text layout is calculated. A
- *   [TextLayoutResult] object that callback provides contains paragraph information, size of the
- *   text, baselines and other details. The callback can be used to add additional decoration or
- *   functionality to the text. For example, to draw selection around the text.
+ * [TextLayoutResult] object that callback provides contains paragraph information, size of the
+ * text, baselines and other details. The callback can be used to add additional decoration or
+ * functionality to the text. For example, to draw selection around the text.
  * @param style style configuration for the text such as color, font, line height etc.
  */
 @Composable
