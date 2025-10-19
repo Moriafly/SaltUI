4f6b59525d7b3de260ec48d2af85618a7d3cb38d
不要糖醋放椒盐 <62319450+Moriafly@users.noreply.github.com>
2025-10-19 10:27:47 +0800

    refactor(platform): Refactor the implementation of the MENUITEMINFO structure

--- ui2/src/desktopMain/kotlin/com/moriafly/salt/ui/platform/windows/structure/MENUITEMINFO.kt
+++ ui2/src/desktopMain/kotlin/com/moriafly/salt/ui/platform/windows/structure/MENUITEMINFO.kt
@@ -31,57 +31,46 @@ import com.sun.jna.platform.win32.WinDef.HMENU
  * - 2025/8/20 Test 1
  */
 @UnstableSaltUiApi
-class MENUITEMINFO :
-    Structure(),
-    Structure.ByReference {
-    @JvmField
-    var cbSize: Int = 0
+@Structure.FieldOrder(
+    "cbSize",
+    "fMask",
+    "fType",
+    "fState",
+    "wID",
+    "hSubMenu",
+    "hbmpChecked",
+    "hbmpUnchecked",
+    "dwItemData",
+    "dwTypeData",
+    "cch",
+    "hbmpItem"
+)
+open class MENUITEMINFO : Structure() {
+    @JvmField var cbSize: Int = 0
 
-    @JvmField
-    var fMask: Int = 0
+    @JvmField var fMask: Int = 0
 
-    @JvmField
-    var fType: Int = 0
+    @JvmField var fType: Int = 0
 
-    @JvmField
-    var fState: Int = 0
+    @JvmField var fState: Int = 0
 
-    @JvmField
-    var wID: Int = 0
+    @JvmField var wID: Int = 0
 
-    @JvmField
-    var hSubMenu: HMENU? = null
+    @JvmField var hSubMenu: HMENU? = null
 
-    @JvmField
-    var hbmpChecked: HBITMAP? = null
+    @JvmField var hbmpChecked: HBITMAP? = null
 
-    @JvmField
-    var hbmpUnchecked: HBITMAP? = null
+    @JvmField var hbmpUnchecked: HBITMAP? = null
 
-    @JvmField
-    var dwItemData: ULONG_PTR = ULONG_PTR(0)
+    @JvmField var dwItemData: ULONG_PTR = ULONG_PTR(0)
 
-    @JvmField
-    var dwTypeData: String? = null
+    @JvmField var dwTypeData: String? = null
 
-    @JvmField
-    var cch: Int = 0
+    @JvmField var cch: Int = 0
 
-    @JvmField
-    var hbmpItem: HBITMAP? = null
+    @JvmField var hbmpItem: HBITMAP? = null
 
-    override fun getFieldOrder(): List<String> = listOf(
-        "cbSize",
-        "fMask",
-        "fType",
-        "fState",
-        "wID",
-        "hSubMenu",
-        "hbmpChecked",
-        "hbmpUnchecked",
-        "dwItemData",
-        "dwTypeData",
-        "cch",
-        "hbmpItem",
-    )
+    class ByReference :
+        MENUITEMINFO(),
+        Structure.ByReference
 }
