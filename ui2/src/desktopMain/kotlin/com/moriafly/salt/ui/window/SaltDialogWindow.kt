9d255f2df26018141c01d173ea420fd71cb78582
不要糖醋放椒盐 <moriafly@163.com>
2025-12-13 00:50:24 +0800

    Fix tool window issue in SaltDialogWindow
    Added a call to updateIsToolWindow(true) to address https://github.com/zeshipalace/Voxzen/issues/120, ensuring the dialog window is correctly treated as a tool window.

--- ui2/src/desktopMain/kotlin/com/moriafly/salt/ui/window/SaltDialogWindow.kt
+++ ui2/src/desktopMain/kotlin/com/moriafly/salt/ui/window/SaltDialogWindow.kt
@@ -184,6 +184,7 @@ fun SaltDialogWindow(
                             }
                         ).apply {
                             updateBorderAndShadow(true)
+                            // Fix https://github.com/zeshipalace/Voxzen/issues/120
                             updateIsToolWindow(true)
                         }
                     }
