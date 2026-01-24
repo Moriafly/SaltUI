ee57967b7c2cce7b300868fcb693058df8dfbcd2
不要糖醋放椒盐 <62319450+Moriafly@users.noreply.github.com>
2026-01-24 17:39:16 +0800

    Opt iOS demo

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,8 +1,9 @@
 @file:Suppress("unused")
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.moriafly.salt.ui.sample.ui.AppContent
 
 @Suppress("FunctionName")
 fun MainViewController() = ComposeUIViewController {
-    MainActivityContent()
+    AppContent()
 }
