5aa70e3cef0696bc8eaa6756868af1eb775b5d5c
Moriafly <moriafly@163.com>
2026-04-29 08:15:19 +0800

    feat: add :navigation module (salt-ui-navigation)
    - New KMP library module for Compose Multiplatform navigation
    - Artifact ID: salt-ui-navigation under io.github.moriafly
    - Depends on :ui2 (salt-ui) and androidx.navigation3
    - Supports Android, Desktop (JVM), and iOS targets
    - Maintains backward compatibility with existing salt-core and salt-ui

---
+++ navigation/build.gradle.kts
@@ -0,0 +1,122 @@
+/*
+ * Salt UI
+ * Copyright (C) 2025 Moriafly
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.plugin.compose)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.vanniktech.maven.publish)
+    `maven-publish`
+}
+
+val isPublishingToMavenLocal = gradle.startParameter.taskNames.any {
+    it.contains("publishToMavenLocal", ignoreCase = true)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.moriafly",
+        artifactId = "salt-ui-navigation",
+        version = if (isPublishingToMavenLocal) "0.0.0-SNAPSHOT" else libs.versions.version.get()
+    )
+
+    pom {
+        name.set("Salt UI Navigation")
+        description.set("Navigation components for Compose Multiplatform (Android/Desktop/iOS)")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Moriafly/SaltUI")
+
+        licenses {
+            license {
+                name.set("Apache-2.0 license")
+                url.set("https://github.com/Moriafly/SaltUI/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("Moriafly")
+                name.set("Moriafly")
+                email.set("moriafly@163.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Moriafly/SaltUI")
+        }
+    }
+
+    publishToMavenCentral()
+    signAllPublications()
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xexpect-actual-classes",
+            "-Xallow-holdsin-contract"
+        )
+    }
+
+    withSourcesJar(false)
+
+    android {
+        namespace = "com.moriafly.salt.ui.navigation"
+        compileSdk = 36
+        minSdk = 23
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+
+        packaging {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            }
+        }
+    }
+
+    jvm("desktop")
+
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "SaltUINavigation"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            api(project(":ui2"))
+            implementation(libs.androidx.navigation3.ui)
+            implementation(libs.androidx.navigationevent.compose)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+
+    targets.withType<KotlinAndroidTarget>().configureEach {
+        publishLibraryVariants("release")
+    }
+}
---
+++ navigation/src/commonMain/kotlin/com/moriafly/salt/ui/navigation/SaltUiNavigation.kt
@@ -0,0 +1,9 @@
+package com.moriafly.salt.ui.navigation
+
+/**
+ * Placeholder for Salt UI Navigation module.
+ *
+ * This module provides navigation components for Compose Multiplatform,
+ * built on top of Salt UI and androidx.navigation3.
+ */
+object SaltUiNavigation
