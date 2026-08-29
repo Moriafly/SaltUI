c0ab01f0d1fac83902da0fb40792a0d5e0677aa3
不要糖醋放椒盐 <62319450+Moriafly@users.noreply.github.com>
2026-08-29 08:53:20 +0800

    refactor(ui2): expose ScreenCard padding defaults

--- ui2/src/commonMain/kotlin/com/moriafly/salt/ui/screen/ScreenCard.kt
+++ ui2/src/commonMain/kotlin/com/moriafly/salt/ui/screen/ScreenCard.kt
@@ -15,21 +15,16 @@
 
 package com.moriafly.salt.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moriafly.salt.ui.Card
-import com.moriafly.salt.ui.Item
-import com.moriafly.salt.ui.ItemArrowType
-import com.moriafly.salt.ui.ItemSwitcher
-import com.moriafly.salt.ui.SaltConfigs
 import com.moriafly.salt.ui.SaltTheme
 import com.moriafly.salt.ui.Text
 import com.moriafly.salt.ui.UnstableSaltUiApi
@@ -61,11 +56,7 @@ fun ScreenCard(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(
-                start = 16.dp,
-                top = 16.dp,
-                end = 16.dp
-            )
+            .padding(ScreenCardDefaults.paddingValues)
     ) {
         header?.let {
             Text(
@@ -103,40 +94,12 @@ fun ScreenCard(
     }
 }
 
-@Preview
-@Composable
-@OptIn(UnstableSaltUiApi::class)
-private fun ScreenCardPreview() {
-    SaltTheme(
-        configs = SaltConfigs.default(
-            isDarkTheme = true
+@UnstableSaltUiApi
+object ScreenCardDefaults {
+    val paddingValues: PaddingValues =
+        PaddingValues(
+            start = 16.dp,
+            top = 16.dp,
+            end = 16.dp
         )
-    ) {
-        Column(
-            modifier = Modifier
-                .background(SaltTheme.colors.background)
-        ) {
-            ScreenCard(
-                header = "播放界面背景图片",
-                footer = "禁用流光而使用自定义的播放界面背景"
-            ) {
-                Item(
-                    onClick = {},
-                    text = "选择图片",
-                    arrowType = ItemArrowType.Link
-                )
-            }
-            ScreenCard {
-                Item(
-                    onClick = {},
-                    text = "Item"
-                )
-                ItemSwitcher(
-                    state = true,
-                    onChange = {},
-                    text = "ItemSwitcher"
-                )
-            }
-        }
-    }
 }
